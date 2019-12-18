c8565620028f34df2afd364d9b3f9f7c6d97aaf7
R00tSAM <49706361+R00tSAM@users.noreply.github.com>
2019-12-18 12:54:05 +0900

    si_samindex.github.io

---
+++ Form01.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>「全てのページにコメントフォーム」の改造｜サンプル</title>
+<link rel="stylesheet" href="/commentform/commentform.css" type="text/css">
+<style type = "text/css">
+body{margin:50px;}
+</style>
+
+</head>
+<body>
+
+<h3>テスト投稿歓迎</h3>
+このページは<a href="http://www.inetagency.net/doc/comment/3/">「全てのページにコメントフォーム」</a>を改造したもののサンプルページです。<a href="commentform.html">解説ページへ</a>
+<hr>
+
+<div id="commentBox"></div>
+<script type="text/javascript" src="/commentform/commentform.js"></script>
+</body>
+</html>
